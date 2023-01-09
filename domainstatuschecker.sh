cedd65ad4d9b69beec4df8568524d363c6f7fcce
DevVelani <122286858+DevVelani@users.noreply.github.com>
2023-01-09 19:49:43 +0530

    Create domainstatuschecker.sh

---
+++ domainstatuschecker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function ctrl_c(){
+    echo -e "\n\n[!] Saliendo...\n"
+    exit 1
+}
+
+trap ctrl_c INT 
+
+output="$1"
+echo -e "\n--------------------------------------------------\n"
+curl -s "https://crt.sh/?q="$output"&output=json" | jq -r '.[] | "\(.name_value)\n\(.common_name)"' | sort -u | while read output
+
+do
+    code=$(curl -s -o /dev/null -w "%{http_code}" $output) 
+    if [ $code -eq 000 ] || [ $code -eq 400 ] || [ $code -eq 403 ] || [ $code -eq 404 ] || [ $code -eq 408 ]|| [ $code -eq 410 ]|| [ $code -eq 422 ]|| [ $code -eq 500 ] || [ $code -eq 502 ]|| [ $code -eq 503 ] || [ $code -eq 504 ]|| [ $code -eq 508 ]|| [ $code -eq 521 ]|| [ $code -eq 525 ]; then
+    	echo "Domain: $output" 
+    	echo "Status: Offline" 
+    	echo "Code: $code"
+    else
+    	echo "Domain: $output"
+    	echo "Status: Online"
+    	echo "Code: $code" 
+    fi
+    
+    echo -e "\n-------Https Verification-------"
+    code=$(curl -s -o /dev/null -w "%{http_code}" https://$output)  
+    if [ $code -eq 000 ] || [ $code -eq 400 ] || [ $code -eq 403 ] || [ $code -eq 404 ] || [ $code -eq 408 ]|| [ $code -eq 410 ]|| [ $code -eq 422 ]|| [ $code -eq 500 ] || [ $code -eq 502 ]|| [ $code -eq 503 ] || [ $code -eq 504 ]|| [ $code -eq 508 ]|| [ $code -eq 521 ]|| [ $code -eq 525 ]; then
+    	echo "$output --> Domain is Offline"
+    	echo "Code: $code"
+    	echo -e "\n--------------------------------------------------\n"
+    else
+    	echo "$output --> Domain is Online"
+    	echo "Code: $code" 
+  	echo -e "\n--------------------------------------------------\n"
+    fi
+
+done
